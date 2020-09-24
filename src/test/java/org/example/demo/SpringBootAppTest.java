9233f4a988edf9cd9f0f09fc2dc0b7766d94ddb2
linux_china <libing.chen@gmail.com>
2020-09-23 17:23:16 -0700

    SpringBootAppTest

---
+++ src/test/java/org/example/demo/SpringBootAppTest.java
@@ -0,0 +1,12 @@
+package org.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+class SpringBootAppTest extends SpringBootBaseTestCase {
+
+    @Test
+    void contextLoads() {
+        System.out.println("It works!");
+    }
+
+}
